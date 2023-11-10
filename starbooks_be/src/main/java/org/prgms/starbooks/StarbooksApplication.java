0e960340b9adf676c26736b993a2cb2a9dd676b6
EomYerim <djadpfa1002@naver.com>
2023-11-11 01:55:32 +0900

    init: 프로젝트 생성

---
+++ starbooks_be/src/main/java/org/prgms/starbooks/StarbooksApplication.java
@@ -0,0 +1,13 @@
+package org.prgms.starbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarbooksApplication.class, args);
+    }
+
+}
---
+++ starbooks_be/src/test/java/org/prgms/starbooks/StarbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prgms.starbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarbooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
