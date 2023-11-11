bc7791a65e0b81a36a5494ad848a59cff537cbfc
EomYerim <djadpfa1002@naver.com>
2023-11-12 01:37:14 +0900

    refactor: 회원 스키마 추가 및 사용하지 않는 test 스키마 제거

--- starbooks_be/src/main/resources/schema.sql
+++ starbooks_be/src/main/resources/schema.sql
@@ -1,10 +1,20 @@
-CREATE TABLE products (
+create TABLE products (
     product_id BINARY(16) PRIMARY KEY,
     product_name VARCHAR(20) NOT NULL,
     category VARCHAR(50) NOT NULL,
     price BIGINT NOT NULL,
     description VARCHAR(500) DEFAULT NULL,
     image_url VARCHAR(500) DEFAULT NULL,
+    created_at DATETIME(6) NOT NULL,
+    updated_at DATETIME(6) DEFAULT NULL
+);
+
+create TABLE members (
+    member_id BINARY(16) PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    address VARCHAR(200) NOT NULL,
+    postcode VARCHAR(20) NOT NULL,
     created_at DATETIME(6) NOT NULL,
     updated_at DATETIME(6) DEFAULT NULL
 );
