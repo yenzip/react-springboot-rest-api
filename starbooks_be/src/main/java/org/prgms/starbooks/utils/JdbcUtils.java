346a9d278cfbbf53fc0fdae60c0e0d97df73c1ac
EomYerim <djadpfa1002@naver.com>
2023-11-11 18:43:52 +0900

    feat: JDBC 관련 util 클래스 생성

---
+++ starbooks_be/src/main/java/org/prgms/starbooks/utils/JdbcUtils.java
@@ -0,0 +1,20 @@
+package org.prgms.starbooks.utils;
+
+import java.nio.ByteBuffer;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class JdbcUtils {
+
+    private JdbcUtils() {}
+
+    public static UUID toUUID(byte[] bytes) {
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+        return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
+    }
+
+    public static LocalDateTime toLocalDateTime(Timestamp timestamp) {
+        return timestamp != null ? timestamp.toLocalDateTime() : null;
+    }
+}
