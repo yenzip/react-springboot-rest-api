1d549d7aafa7f0a9212b776c303f8d73f3554c4a
EomYerim <djadpfa1002@naver.com>
2023-11-11 18:43:10 +0900

    feat: 상품 model 생성

---
+++ starbooks_be/src/main/java/org/prgms/starbooks/model/product/Product.java
@@ -0,0 +1,83 @@
+package org.prgms.starbooks.model.product;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Product {
+    private final UUID productId;
+    private String productName;
+    private Category category;
+    private long price;
+    private String description;
+    private String imageUrl;
+    private final LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Product(UUID productId, String productName, Category category, long price, String description, String imageUrl, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+        this.price = price;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+        this.updatedAt = LocalDateTime.now();
+    }
+}
