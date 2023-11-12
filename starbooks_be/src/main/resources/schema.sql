32704962a4a10bd2bc84cf79574d4ac747390d4e
EomYerim <djadpfa1002@naver.com>
2023-11-12 23:25:22 +0900

    refactor: 주문, 주문 상품 스키마 생성

--- starbooks_be/src/main/resources/schema.sql
+++ starbooks_be/src/main/resources/schema.sql
@@ -18,3 +18,26 @@ create TABLE members (
     created_at DATETIME(6) NOT NULL,
     updated_at DATETIME(6) DEFAULT NULL
 );
+
+create TABLE orders (
+    order_id     binary(16) PRIMARY KEY,
+    member_id    binary(16) NOT NULL,  -- 회원과 주문의 관계를 나타내는 외래 키
+    order_status VARCHAR(50)  NOT NULL,
+	address VARCHAR(200) NOT NULL,
+    postcode VARCHAR(20) NOT NULL,
+    created_at   datetime(6)  NOT NULL,
+    updated_at   datetime(6)  DEFAULT NULL,
+    FOREIGN KEY (member_id) REFERENCES members(member_id)
+);
+
+create TABLE order_items (
+    seq        bigint      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    order_id   binary(16)  NOT NULL,
+    product_id binary(16)  NOT NULL,
+    category   VARCHAR(50) NOT NULL,
+    price      bigint      NOT NULL,
+    quantity   int         NOT NULL,
+    INDEX (order_id),
+    CONSTRAINT fk_order_items_to_order FOREIGN KEY (order_id) REFERENCES orders (order_id) ON delete CASCADE,
+    CONSTRAINT fk_order_items_to_product FOREIGN KEY (product_id) REFERENCES products (product_id)
+);
